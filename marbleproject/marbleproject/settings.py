de1cc783672c37dc1460143ed1201bb6b9bd36c6
Ahmet Karakaya <karakayahmet00@gmail.com>
2022-11-14 13:21:12 +0300

    bootstrap kütüphaneleri eklendi.

--- marbleproject/marbleproject/settings.py
+++ marbleproject/marbleproject/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = "marbleproject.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / 'templates'
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [ 
+    BASE_DIR / "static" 
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
